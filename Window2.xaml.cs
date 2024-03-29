18bf3b904295aa0b6431d3219824d7b8e73f290e
Diana <izabellaslivina@gmail.com>
2024-03-29 17:38:51 +0300

    Добавьте файлы проекта.

---
+++ Window2.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EFLab1
+{
+    /// <summary>
+    /// Логика взаимодействия для Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        private RecordingStudioPractVersionEntities context = new RecordingStudioPractVersionEntities();
+        public Window2()
+        {
+            InitializeComponent();
+            StaffDataGrid.ItemsSource = context.Staff.ToList();
+
+        }
+        private void ReturnButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+        private void ForwardButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window3 window3 = new Window3();
+            window3.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Window3.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EFLab1
+{
+    /// <summary>
+    /// Логика взаимодействия для Window3.xaml
+    /// </summary>
+    public partial class Window3 : Window
+    {
+        private RecordingStudioPractVersionEntities context = new RecordingStudioPractVersionEntities();
+
+        public Window3()
+        {
+            InitializeComponent();
+            StaffChoiceDataGrid.ItemsSource = context.Staff_Choice.ToList();
+        }
+        private void ReturnButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 window2 = new Window2();
+            window2.Show();
+            this.Close();
+        }
+    }
+}
